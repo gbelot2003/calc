3ce8dd66bfd662c2b6290b172d95ba8f02e75c82
Gerardo Belot <gbelot2003@hotmail.com>
2018-04-12 14:41:41 -0600

    agregando switch

--- resources/views/dashboard/panels/_search.blade.php
+++ resources/views/dashboard/panels/_search.blade.php
@@ -16,4 +16,9 @@
 @if(isset($buscar) || isset($desde) && isset($hasta))
     <a href="/admin/dashboard/export/excel{{ isset($buscar) ? '/'.$buscar : '/null' }}{{ isset($desde) ? '/'.$desde : '/null' }}{{ isset($hasta) ? '/'.$hasta : '/null' }}" class="btn btn-warning"><i class="fa fa-file-excel-o" aria-hidden="true"></i> Exportar Excel</a>
 @endif
+<div style="text-align: right">
+    <a class="btn btn-danger" href="/admin/dashboard/deleteit/all" onclick="return confirm('Are you sure you want to search Google?')">Eliminar Todo</a>
+</div>
+
+
 {!! Form::close() !!}
