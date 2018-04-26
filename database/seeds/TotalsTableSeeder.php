cefb79243588b1eb9d8d846ec51d66c56cdc459f
Gerardo Belot <gbelot2003@hotmail.com>
2018-04-26 10:44:18 -0600

    cambios en seed de Totales

--- database/seeds/TotalsTableSeeder.php
+++ database/seeds/TotalsTableSeeder.php
@@ -27,33 +27,23 @@ class TotalsTableSeeder extends Seeder
                 'updated_at' => '2017-05-03 01:19:28',
                 'subtitle' => 'Normal',
             ),
-            1 => 
-            array (
-                'id' => 2,
-                'name' => 'Cuota por el programa DC a tu alcance 5%
-',
-                'porcent' => 5.0,
-                'created_at' => '2017-05-03 01:19:31',
-                'updated_at' => '2017-05-03 01:19:32',
-                'subtitle' => NULL,
-            ),
-            2 => 
+            1 =>
             array (
                 'id' => 3,
-                'name' => 'Con tarjeta de DC BAC Credomatic Economía 7%',
+                'name' => 'Con tarjeta de BAC Credomatic Economía 7%',
                 'porcent' => 7.0,
                 'created_at' => '2017-05-03 01:19:33',
                 'updated_at' => '2017-05-03 01:19:32',
-                'subtitle' => 'Credomatic Economia 7% Descuento',
+                'subtitle' => 'Credomatic Economía 7% Descuento',
             ),
-            3 => 
+            2 =>
             array (
                 'id' => 4,
-                'name' => 'Débito automático FICOHSA o DC Credomatich 10%',
+                'name' => 'Débito automático FICOHSA Credomatic 10%',
                 'porcent' => 10.0,
                 'created_at' => '2017-05-03 01:19:35',
                 'updated_at' => '2017-05-03 01:19:34',
-                'subtitle' => 'Debito Automatico',
+                'subtitle' => 'Débito automático 10%',
             ),
         ));
         
